152c69dd5b72feb21aa4e54b070c69adf5e2e20d
Alex Henrique <alexsctj@gmail.com>
2020-10-17 12:19:00 -0300

    quinto arquivo

--- 004 - adjacentElementsProduct.js
+++ 004 - adjacentElementsProduct.js
@@ -1,11 +1,8 @@
 /*
 Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
-
 Example
-
 For inputArray = [3, 6, -2, -5, 7, 3], the output should be
 adjacentElementsProduct(inputArray) = 21.
-
 7 and 3 produce the largest product.
 */
 
@@ -13,7 +10,7 @@ function adjacentElementsProduct(inputArray) {
   let tam = inputArray.length;
   let max = inputArray[0] * inputArray[1];
   for (i = 1; i < tam; i++) {
-    let calc = inputArray[i] * inputArray[i + 1];
+    let calc = inputArray[i] * inputArray[i 1 1];
     if (max < calc) {
       max = calc;
     }
